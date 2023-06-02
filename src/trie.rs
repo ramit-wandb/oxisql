f5616734f3f34859ce046c0ccc228ce21fdd28b2
Ramit Goolry <ramit@wandb.com>
2023-06-02 03:03:27 -0700

    Added recency to trie

--- src/trie.rs
+++ src/trie.rs
@@ -2,27 +2,76 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Trie {
-    children: HashMap<char, Trie>,
-    word: Option<String>
+    root: TrieNode,
+    index : usize
+    // TODO Modes - alphabetical (default), recency, frequency, frecency
 }
 
 impl Trie {
     pub fn new() -> Trie {
         Trie {
-            children: HashMap::new(),
-            word: None
+            root: TrieNode::new(),
+            index: 0
         }
     }
 
     pub fn insert(&mut self, word: &str) {
-        let mut node = self;
-        for c in word.chars() {
-            node = node.children.entry(c).or_insert(Trie::new());
-        }
-        node.word = Some(word.to_string());
+        self.root.insert(word, self.index);
+        self.index += 1;
     }
 
     pub fn search_all(&self, prefix: &str) -> Vec<String> {
+        let mut vector = self.root.search_all(prefix);
+
+        // Sort by index
+        vector.sort_by(|a, b| {
+            let a_index = self.get_index(a).unwrap();
+            let b_index = self.get_index(b).unwrap();
+            a_index.cmp(&b_index)
+        });
+
+        vector
+    }
+
+    fn get_index(&self, word: &str) -> Option<usize> {
+        let mut node = &self.root;
+        for c in word.chars() {
+            if let Some(child) = node.children.get(&c) {
+                node = child;
+            } else {
+                return None;
+            }
+        }
+        node.index
+    }
+}
+
+#[derive(Debug)]
+struct TrieNode {
+    children: HashMap<char, TrieNode>,
+    word: Option<String>,
+    index: Option<usize>
+}
+
+impl TrieNode {
+    fn new() -> TrieNode {
+        TrieNode {
+            children: HashMap::new(),
+            word: None,
+            index: None
+        }
+    }
+
+    fn insert(&mut self, word: &str, index: usize) {
+        let mut node = self;
+        for c in word.chars() {
+            node = node.children.entry(c).or_insert(TrieNode::new());
+        }
+        node.word = Some(word.to_string());
+        node.index = Some(index);
+    }
+
+    fn search_all(&self, prefix: &str) -> Vec<String> {
         let mut node = self;
         for c in prefix.chars() {
             if let Some(child) = node.children.get(&c) {
@@ -36,7 +85,7 @@ impl Trie {
         words.clone()
     }
 
-    pub fn traverse(&self, words: &mut Vec<String>) {
+    fn traverse(&self, words: &mut Vec<String>) {
         if let Some(word) = &self.word {
             words.push(word.clone());
         }
